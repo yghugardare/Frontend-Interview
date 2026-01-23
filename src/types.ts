b306c846f89e43051ff3099b52cfa25695aa4577
ibharathboga <ibharathboga@gmail.com>
2026-01-23 08:51:18 +0530

    chore(types): add initial blog domain types

---
+++ src/types.ts
@@ -0,0 +1,11 @@
+export interface Blog {
+  id: string;
+  title: string;
+  category: string[];
+  description: string;
+  date: string;
+  coverImage: string;
+  content: string;
+}
+
+export type CreateBlogPayload = Omit<Blog, "id">;
