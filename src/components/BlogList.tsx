d307c16566dc1534d24c1c4f608a9c4116dfd4a2
ibharathboga <ibharathboga@gmail.com>
2026-01-23 23:05:59 +0530

    blog list, use isPending instead of isLoading wip

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -1,20 +1,37 @@
+import { useEffect } from "react";
 import BlogItem from "./BlogItem";
+import { useBlogs } from "@/hooks/blogs";
 
-export default function BlogList() {
+export default function BlogItemList() {
+  const { data, isPending, isError } = useBlogs();
+
+  useEffect(() => {
+    if (isPending) {
+      console.log("isPending");
+      return;
+    }
+    console.log(data);
+  }, [data, isPending]);
+
+  if (isPending) {
+    return <div>Loading skeletons...</div>;
+  }
+
+  if (isError) {
+    return <div>Failed to load blogs</div>;
+  }
+
   return (
-    <div className="flex flex-col space-y-2">
-      <BlogItem />
-      <BlogItem />
-      <BlogItem />
-      <BlogItem />
-      <BlogItem />
-      <BlogItem />
-      <BlogItem />
-      <BlogItem />
-      <BlogItem />
-      <BlogItem />
-      <BlogItem />
-      <BlogItem />
+    <div className="flex flex-col space-y-4">
+      {data?.map((blog) => (
+        <BlogItem
+          key={blog.id}
+          title={blog.title}
+          description={blog.description}
+          categories={blog.category}
+          date={blog.date}
+        />
+      ))}
     </div>
   );
 }
