aa76e85c815b1ac3ebc2dbcc88aff71933365f1a
ibharathboga <ibharathboga@gmail.com>
2026-01-23 21:55:36 +0530

    blog item wip

--- src/components/BlogItem.tsx
+++ src/components/BlogItem.tsx
@@ -1,26 +1,55 @@
 import {
   Card,
-  CardAction,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from "@/shadcn/components/ui/card";
 
-export default function BlogItem() {
+interface BlogItemProps {
+  id: string;
+  title: string;
+  description: string;
+  footer: string;
+}
+
+// this is ts file
+// in wip
+// sample blog post looks like this
+const blogSample = {
+  id: "9",
+  title: "Digital Transformation in Accounting Firms",
+  category: ["TECH", "CAREER"],
+  description: "How firms are adapting to cloud-based accounting",
+  date: "2025-12-15T09:00:00.000Z",
+  coverImage:
+    "https://images.pexels.com/photos/3184465/pexels-photo-3184465.jpeg",
+  content: "Accounting ...............",
+};
+// for blog item, content and cover image is not needed
+// top left using shadcn badges we showcase category
+// top right date
+// remaining appropriate
+// make necessary changes
+
+export default function BlogItem({
+  id,
+  title,
+  description,
+  footer,
+}: BlogItemProps) {
   return (
-    <Card>
+    <Card key={id}>
       <CardHeader>
-        <CardTitle>Card Title</CardTitle>
-        <CardDescription>Card Description</CardDescription>
-        <CardAction>Card Action</CardAction>
+        <CardTitle>{title}</CardTitle>
       </CardHeader>
+
       <CardContent>
-        <p>Card Content</p>
+        <p>{description}</p>
       </CardContent>
+
       <CardFooter>
-        <p>Card Footer</p>
+        <p>{footer}</p>
       </CardFooter>
     </Card>
   );
