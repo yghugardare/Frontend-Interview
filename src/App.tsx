b3eed50234aad4d185eddd6bc3e9444028e290c9
ibharathboga <ibharathboga@gmail.com>
2026-01-23 21:20:03 +0530

    App.tsx: wip: setup temporary layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import { useState } from "react";
+import BlogList from "./components/BlogList";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
-    </>
+    <div className="h-screen flex flex-col ">
+      <main className="h-full flex">
+        <aside className="w-full lg:max-w-90 shrink-0 overflow-auto p-2">
+          <BlogList />
+        </aside>
+        <section className="hidden lg:block grow bg-blue-300 overflow-auto">
+          <p>right panel</p>
+          <p>Right panel content will trigger its own scroll</p>
+          <div className="h-[100px] w-[1000vh] bg-violet-300" />
+          <p>Demo tall content </p>
+          <div className="h-[100vh] bg-purple-200" />
+        </section>
+      </main>
+    </div>
   );
 }
 
