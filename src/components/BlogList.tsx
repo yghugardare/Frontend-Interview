735cf2b2e079c37a6f28328be6aed558eced4a29
ibharathboga <ibharathboga@gmail.com>
2026-01-24 10:10:04 +0530

    BlogList, add button to new blog form in blog list

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -1,9 +1,10 @@
 import { useEffect } from "react";
+import { Link } from "react-router";
 import BlogItem from "./BlogItem";
 import { useBlogs } from "@/hooks/blogs";
 import BlogItemSkeleton from "./BlogItemSkeleton";
 import { Button } from "@/shadcn/components/ui/button";
-import { ArrowLeft } from "lucide-react";
+import { Plus as PlusIcon } from "lucide-react";
 
 export default function BlogItemList() {
   const { data, isPending, isError } = useBlogs();
@@ -39,17 +40,24 @@ export default function BlogItemList() {
   }
 
   return (
-    <div className="flex flex-col space-y-4">
-      {data?.map((blog) => (
-        <BlogItem
-          key={blog.id}
-          id={blog.id}
-          title={blog.title}
-          description={blog.description}
-          categories={blog.category}
-          date={blog.date}
-        />
-      ))}
+    <div className="flex flex-col h-full">
+      <Button asChild variant="default" size="lg">
+        <Link to="/blogs/new">
+          <PlusIcon className="mr-2 h-4 w-4" /> Add New Blog
+        </Link>
+      </Button>
+      <div className="flex-1 flex flex-col space-y-2 overflow-auto  p-2">
+        {data?.map((blog) => (
+          <BlogItem
+            key={blog.id}
+            id={blog.id}
+            title={blog.title}
+            description={blog.description}
+            categories={blog.category}
+            date={blog.date}
+          />
+        ))}
+      </div>
     </div>
   );
 }
